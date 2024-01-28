d71e3eda98f8b5aa9b74101774337605e260d3b0
Dave Jung <davejinyoung@gmail.com>
2024-01-27 22:44:59 -0800

    Better marker functionality and cleaned up code.

--- looper/home/static/home/mapbox.js
+++ looper/home/static/home/mapbox.js
@@ -1,20 +1,28 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGF2ZWppbnlvdW5nIiwiYSI6ImNscm84czI4ajA3ZHYya2w5c29wZmhwdWsifQ.4EwYfetiww7nb40hQV_RNQ';
 
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-123.12, 49.28], // starting position [lng, lat]
-    zoom: 10, // starting zoom
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [-123.12, 49.28],
+    zoom: 10,
 });
 
-map.addControl(
-    new MapboxGeocoder({
-        accessToken: mapboxgl.accessToken,
-        mapboxgl: mapboxgl
-    }),
-    'top-left'
-);
+let curMarker;
+let geolocationActive = false;
 
+// Script for searching for location
+const geocoder = new MapboxGeocoder({
+    accessToken: mapboxgl.accessToken,
+    mapboxgl: mapboxgl,
+});
+map.addControl(geocoder);
+geocoder.on('result', (event) => {
+    const coordinates = event.result.geometry.coordinates;
+    addNewMarker(coordinates);
+    geocoder.clear();
+});
+
+// Script for finding current location
 const geolocateControl = new mapboxgl.GeolocateControl({
     positionOptions: {
         enableHighAccuracy: true
@@ -22,25 +30,30 @@ const geolocateControl = new mapboxgl.GeolocateControl({
     trackUserLocation: true,
     showUserHeading: true
 });
-
 map.addControl(geolocateControl);
-
 document.getElementById('btn-check').addEventListener('click', function(event) {
     geolocateControl.trigger();
 });
 
+// Script for location of clicked area on map
+geolocateControl.on('geolocate', (event) => {
+    const coordinates = [event.coords.longitude, event.coords.latitude];
+    addNewMarker(coordinates);
+});
+map.on('click', (event) => {
+    const coordinates = [event.lngLat.lng, event.lngLat.lat];
+    addNewMarker(coordinates);
+});
 
-
-let curMarker;
-map.on('click', evt => {
-    console.log(evt.lngLat);
+// Adds a new marker
+function addNewMarker(coordinates) {
     if(curMarker != null){
         curMarker.remove();
     }
     const marker = new mapboxgl.Marker()
-        .setLngLat([evt.lngLat.lng, evt.lngLat.lat])
-        .setPopup(new mapboxgl.Popup().setHTML(`<p>Latitude is: ${evt.lngLat.lat}, Longitude is: ${evt.lngLat.lng}</p>`)) // add popup
+        .setLngLat(coordinates)
+        .setPopup(new mapboxgl.Popup().setHTML(`<p>${coordinates}</p>`))
         .addTo(map);
     marker.togglePopup();
     curMarker = marker;
-});
+}
