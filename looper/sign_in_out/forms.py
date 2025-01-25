cb21ccb1da85b9c7b128e75c6e1d158e6ad2b3e6
davejinyoung <davejinyoung@gmail.com>
2025-01-24 21:56:40 -0800

    Signup form UI updates

--- looper/sign_in_out/forms.py
+++ looper/sign_in_out/forms.py
@@ -9,6 +9,12 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2', 'address']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].help_text = ''
+        self.fields['password1'].help_text = ''
+        self.fields['password2'].help_text = ''
 
     def save(self, commit=True):
         user = super().save(commit=False)
